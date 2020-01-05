d04bbbc4afeeb9c39d347fe09512a45bc09f77e9
abdulrahmanafa <abdulrahmanafa@moe.edu.kw>
2020-01-05 13:52:32 +0300

    Initial Commit

---
+++ label/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  label
+//
+//  Created by Abdulrhman A F  Alomairi on 1/5/20.
+//  Copyright © 2020 Abdulrhman A F  Alomairi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
